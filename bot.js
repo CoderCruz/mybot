4ad4d858dec015312a2780a083adbc89e0863ee8
Jose Cruz <cruzjosework@gmail.com>
2020-09-15 00:56:21 -0400

    added features where you can add foo argument after running command. the bot will look and check if arguments match. and then will return bar if its the argument we are looking for

--- bot.js
+++ bot.js
@@ -13,12 +13,33 @@ client.on('message', message => {
     //command message.content is how to access content
     // console.log(message.content)
 
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    //this is how we would create and access our commands
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
 
         //startsWith will allow you to add more commands after main command.
     if(message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('Pong. ')
     } else if (message.content === `${prefix}beep`) {
         message.channel.send('Boop')
+    } else if(message.content === `${prefix}server`) {
+        message.channel.send(`This server's name is ${message.guild.name}\nTotal members: ${message.guild.memberCount}`)
+    } else if (message.content === `${prefix}user-info`) {
+        message.channel.send(`your username: ${message.author.username}\nYour ID: ${message.author.id}`)
+    } else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+        else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+
+	    message.channel.send(`First argument: ${args[0]}`);
+    
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
     }
 })
 
